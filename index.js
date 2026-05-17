5cedbc6fbf067a53275326f23f715307282444d9
souravnath4587-cmd <souravnath4587@gmail.com>
2026-05-18 01:52:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const app = express();
+
+const uri =
+  "mongodb+srv://pet-nest:GlUTpYzfwVUtEo9S@cluster0.l69kqn7.mongodb.net/?appName=Cluster0";
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db("petNest");
+    const petCollection = db.collection("allPets");
+
+    // All Pet information get on this..
+    app.get("/pet", async (req, res) => {
+      const result = await petCollection.find().toArray();
+      res.json(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("The Pet server is connected...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
